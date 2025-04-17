3aa86b5fde226e6c396753c124cc1ef60cb06905
manuttall <111887980+manuttall@users.noreply.github.com>
2025-04-17 11:39:58 -0600

    Updated Vector to handle Vertex and scalar operations

--- renderer/geometry/vector.py
+++ renderer/geometry/vector.py
@@ -1,13 +1,14 @@
 """Vector class for vector operations in 3D space"""
 
+from __future__ import annotations
+import math
+from geometry.vertex import Vertex
+
 __author__ = "Michael Nuttall"
 __date__ = "2025/04/16"
 __license__ = "MIT"
 __version__ = "0.1.0"
 __maintainer__ = "Michael Nuttall"
-
-import math
-from geometry.vertex import Vertex
 
 
 class Vector(Vertex):
@@ -31,7 +32,7 @@ class Vector(Vertex):
         """
         return math.sqrt(self._x**2 + self._y**2 + self._z**2)
 
-    def normalize(self) -> "Vector":
+    def normalize(self) -> Vector:
         """Returns the unit (normalized) vector.
 
         Returns:
@@ -42,7 +43,7 @@ class Vector(Vertex):
             raise ValueError("Cannot normalize a zero vector.")
         return Vector(self._x / mag, self._y / mag, self._z / mag)
 
-    def dot(self, other: "Vector") -> float:
+    def dot(self, other: Vector) -> float:
         """Computes the dot product with another vector.
 
         Args:
@@ -55,7 +56,7 @@ class Vector(Vertex):
             raise TypeError("Dot product requires another Vector.")
         return self._x * other.x + self._y * other.y + self._z * other.z
 
-    def cross(self, other: "Vector") -> "Vector":
+    def cross(self, other: Vector) -> Vector:
         """Computes the cross product with another vector.
 
         Args:
@@ -72,27 +73,82 @@ class Vector(Vertex):
         z = self._x * other.y - self._y * other.x
         return Vector(x, y, z)
 
-    def __add__(self, other: "Vector") -> "Vector":
-        """Vector addition
+    def __add__(self, other: Vertex) -> Vertex | Vector:
+        """Add this vector to another vector or vertex.
 
         Args:
-            other (Vector): another vector
+            other (Vertex): the other vector or vertex to add
 
         Returns:
-            Vector: the resulting vector
+            Vector | Vertex: a new Vector if the other is a Vector,
+                            a new Vertex if the other is a Vertex
         """
-        return Vector(self._x + other.x, self._y + other.y, self._z + other.z)
+        if isinstance(other, Vector):
+            return Vector(self._x + other.x, self._y + other.y, self._z + other.z)
+        elif isinstance(other, Vertex):
+            return Vertex(self._x + other.x, self._y + other.y, self._z + other.z)
+        return NotImplemented
 
-    def __sub__(self, other: "Vector") -> "Vector":
-        """Vector subtraction
+    def __radd__(self, other: Vertex) -> Vertex | Vector:
+        """Add this vector to another object from the right-hand side.
 
         Args:
-            other (Vector): another vector
+            other (Vertex): the left-hand operand (typically a Vertex)
 
         Returns:
-            Vector: the resulting vector
+            Vector | Vertex: the result of the addition
         """
-        return Vector(self._x - other.x, self._y - other.y, self._z - other.z)
+        return self.__add__(other)
+
+    def __sub__(self, other: Vertex) -> Vector:
+        """Subtract a vector or vertex from this vector.
+
+        Args:
+            other (Vertex): the vector or vertex to subtract
+
+        Returns:
+            Vector: a new vector representing the difference
+        """
+        if isinstance(other, Vector) or isinstance(other, Vertex):
+            return Vector(self._x - other.x, self._y - other.y, self._z - other.z)
+        return NotImplemented
+
+    def __rsub__(self, other: Vertex) -> Vector:
+        """Subtract this vector from a vertex (right-hand subtraction).
+
+        Args:
+            other (Vertex): the left-hand operand
+
+        Returns:
+            Vector: a new vector representing the difference
+        """
+        if isinstance(other, Vertex):
+            return Vector(other.x - self._x, other.y - self._y, other.z - self._z)
+        return NotImplemented
+
+    def __mul__(self, scalar: float) -> Vector:
+        """Multiply this vector by a scalar.
+
+        Args:
+            scalar (float): the scalar to multiply by
+
+        Returns:
+            Vector: a new vector scaled by the given scalar
+        """
+        if not isinstance(scalar, (int, float)):
+            return NotImplemented
+        return Vector(self._x * scalar, self._y * scalar, self._z * scalar)
+
+    def __rmul__(self, scalar: float) -> Vector:
+        """Multiply this vector by a scalar from the left-hand side.
+
+        Args:
+            scalar (float): the scalar to multiply by
+
+        Returns:
+            Vector: a new vector scaled by the given scalar
+        """
+        return self.__mul__(scalar)
 
     def __eq__(self, other: object) -> bool:
         """Check equality with another vector.
